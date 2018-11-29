56d32b029b008a20a1a8157b9dfda992b139f2cb
takutohfl <mogerio2626@gmail.com>
2018-11-29 06:02:24 +0000

    Add new wall_stop.py

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -4,14 +4,14 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WaiiStop():
+class WallStop():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
         self.sensor_values = LightSensorValues()
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
 
-    def callback(self,message):
+    def callback(self,messages):
         self.sensor_values = messages
 
     def run(self):
